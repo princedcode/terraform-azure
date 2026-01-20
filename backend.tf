cb0631571d4d3e1c35247ca168d36e1b29763faa
princedcode <princedavidplr@gmail.com>
2026-01-20 14:48:38 -0700

    Change backend key in backend.tf
    Updated the key for the Azure backend configuration.

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     resource_group_name  = "rg-tf-state"
     storage_account_name = "tfstate12302025"
     container_name       = "tfstate"
-    key                  = ""
+    key                  = "LigMGjmtiIQBz9cTfSRp9hGNSKUbPYqgku0EalhM5re+1jZwTXj9j/Y1vGmSASZeCT4YHMdXr2zN+AStJbjocQ=="
   }
 }
