7c065cb9344e32e279543c24ba1c9e1c58c5d4f9
princedcode <princedavidplr@gmail.com>
2026-01-21 10:21:19 -0700

    Remove key from Azure backend configuration
    Clear the key value for the Azure backend configuration.

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     resource_group_name  = "rg-tf-state"
     storage_account_name = "tfstate12302025"
     container_name       = "tfstate"
-    key                  = "HhfkJmFxAcXwaWjONB65XK3rEXHpkAvSqn9PHnWZGnIg/GRKoGOqQsnFfrF29rcaDQ5e0ohZChW/+AStN9Esrw=="
+    key                  = ""
   }
 }
